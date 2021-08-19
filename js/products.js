81812cdc2c3979c1044710d6b608d358d295944e
gavafue <88394722+gavafue@users.noreply.github.com>
2021-08-19 14:04:30 -0300

    cambio de variable

--- js/products.js
+++ js/products.js
@@ -1,16 +1,16 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-var probando = "";
+var contenedor = "";
 document.addEventListener("DOMContentLoaded", function (e) {
         fetch(PRODUCTS_URL)
             .then(respuesta => respuesta.json())
 
             .then(datos => {
                 datos.forEach(element => {
-                    probando += `<div class="cajita"><div class="imagen" style="background:url(${element.imgSrc})"></div><span>${element.name}</span><p>${element.description}</p><div class="precio">${element.currency} ${element.cost}</div></div>`
-                    document.getElementById("listadoproductos").innerHTML = probando;
+                    contenedor += `<div class="cajita"><div class="imagen" style="background:url(${element.imgSrc})"></div><span>${element.name}</span><p>${element.description}</p><div class="precio">${element.currency} ${element.cost}</div></div>`
                 });
+                document.getElementById("listadoproductos").innerHTML = contenedor;
 
             })
             .catch(error => alert("Hubo un error: " + error));
