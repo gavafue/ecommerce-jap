84f970eff190bc13a876aa0fc5c510a18707ec28
gavafue <88394722+gavafue@users.noreply.github.com>
2021-08-19 12:58:23 -0300

    redireccion de login

--- js/init.js
+++ js/init.js
@@ -39,7 +39,9 @@ var getJSONData = function(url){
         return result;
     });
 }
-
+function redireccionar(url){
+  window.location = url;
+}
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
