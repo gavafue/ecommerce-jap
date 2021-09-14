b4ab0367d95ec300630978aa8f0835f5406a52f9
gavafue <88394722+gavafue@users.noreply.github.com>
2021-09-14 11:20:11 -0300

    comentarios del producto

--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,63 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+function showComments(array){
+var contenedor = "";
+array.forEach(element => {
+    var stars=""
+    if(element.score==0){
+        stars= `<span class="fa fa-star "></span>
+        <span class="fa fa-star "></span>
+        <span class="fa fa-star "></span>
+        <span class="fa fa-star"></span>
+        <span class="fa fa-star"></span>`
+    }
+    if(element.score==1){
+        stars=`<span class="fa fa-star checked"></span>
+        <span class="fa fa-star "></span>
+        <span class="fa fa-star "></span>
+        <span class="fa fa-star"></span>
+        <span class="fa fa-star"></span>`
+    }
+    if(element.score==2){
+        stars=`<span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star "></span>
+        <span class="fa fa-star"></span>
+        <span class="fa fa-star"></span>`
+    }
+    if(element.score==3){
+        stars=`<span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star"></span>
+        <span class="fa fa-star"></span>`
+    }
+    if(element.score==4){
+        stars=`<span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star"></span>`
+    }
+    if(element.score==5){
+        stars=`<span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>`
+    }
+    contenedor += `<div class="comment"><b>${element.user}</b> ${element.description}
+  <div class="stars"> <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+${stars}
+ <p>${element.dateTime}</p></div></div><hr>`
+                    });
+                    document.getElementById("commentslist").innerHTML = contenedor;
+                }
+
+
+
+            
 document.addEventListener("DOMContentLoaded", function(e){
     fetch(PRODUCT_INFO_URL)
     .then(respuesta => respuesta.json())
@@ -16,5 +73,9 @@ document.addEventListener("DOMContentLoaded", function(e){
         <img src="${datos.images[3]}" width="220px"></img></div>
        <hr>`;
         
+    });
+
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (result) {
+       showComments(result.data);
     });
 });
