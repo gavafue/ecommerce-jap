d37dafee5a04cb389bbd80ae6aa977b9f64f32af
gavafue <88394722+gavafue@users.noreply.github.com>
2021-09-29 13:06:37 -0300

    Carrousel para fotos de info-products.html

--- js/product-info.js
+++ js/product-info.js
@@ -131,11 +131,37 @@ arrayproductos = result.data;
         <h2>${datos.name}</h2></div>
         <p style="text-align:justify;">${datos.description}</p>
         <p style="text-align:center;font-weight:bold;">${datos.currency} ${datos.cost}</p>
-        <div style="margin:0 auto;width:90%;"><img src="${datos.images[0]}" width="220px"></img>
-        <img src="${datos.images[1]}" width="220px"></img>
-        <img src="${datos.images[2]}" width="220px"></img>
-        <img src="${datos.images[3]}" width="220px"></img></div>
+        <div class="col-9" style="margin:0 auto;">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        
+   <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="${datos.images[1]}" class="d-block w-100" alt="Primera imagen de ${datos.name}">
+          </div>
+          <div class="carousel-item">
+            <img src="${datos.images[2]}" class="d-block w-100" alt="Segunda imagen de ${datos.name}">
+          </div>
+          <div class="carousel-item">
+            <img src="${datos.images[3]}" class="d-block w-100" alt="Tercera imagen de ${datos.name}">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previo</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Siguiente</span>
+        </a>
+      </div></div>
        <hr>`;
+
+
             datos.relatedProducts.forEach(element => {
                 document.getElementById("productosRelacionados").innerHTML += `
                 <div class="card" style="max-width: 200px !important;">
