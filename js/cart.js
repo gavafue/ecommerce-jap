652f1460889899411bcd5de43ceec949e338eb2e
gavafue <88394722+gavafue@users.noreply.github.com>
2021-10-15 08:30:08 -0300

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -5,27 +5,28 @@ var listadocarrito
 let counter = 0;
 
 document.addEventListener("DOMContentLoaded", function (e) {
-    getJSONData(CART_2_CURRENCY).then(function (result) {
-        if (result.status === "ok") {
-            listadocarrito = result.data.articles;
-        }
+  getJSONData(CART_2_CURRENCY).then(function (result) {
+    if (result.status === "ok") {
+      listadocarrito = result.data.articles;
+    }
 
-        for (let i = 0; i < listadocarrito.length; i++) {
-            let element = listadocarrito[i];
-            let sub = element.unitCost * element.count
-            counter++;
-            document.getElementById("listacarrito").innerHTML += `<tr>
+    for (let i = 0; i < listadocarrito.length; i++) {
+      let element = listadocarrito[i];
+      let sub = element.unitCost * element.count
+      counter++;
+      document.getElementById("listacarrito").innerHTML += `<tr>
             <th scope="row">${counter}</th>
             <td><img src="${element.src}"style="width:60px;"></td>
             <td>${element.name}</td>
             <td>${element.unitCost} ${element.currency}</td>
-            <td><div class="input-group mb-3 mx-auto" style="width:30% !important;"><input type="Number" id="cantidad${[i]}" onchange="modificarSubtotal(${element.unitCost},${i})" class="mx-auto form-control" placeholder="Cantidad" value="${element.count}" aria-label="Username" aria-describedby="addon-wrapping" min="0";></div></td>
-            <td>${sub} ${element.currency}</td>
+            <td>
+            <div class="input-group mb-3 mx-auto" style="width:30% !important;"><input type="Number" id="cantidad${[i]}" onchange="modificarSubtotal(${element.unitCost},${i})" class="mx-auto form-control" placeholder="Cantidad" value="${element.count}" aria-label="Username" aria-describedby="addon-wrapping" min="0";></div></td>
+            <td id="subtotal${[i]}">${sub} ${element.currency}</td>
           </tr>  `;
-        }
+    }
 
 
-        document.getElementById("zonapago").innerHTML = `
+    document.getElementById("zonapago").innerHTML = `
        <div class="row"><div class="col"> <div class="table-responsive">
         <table class="table table-striped table-hover table-borderless table-sm table-dark ">
             <tbody>
@@ -73,6 +74,6 @@ document.addEventListener("DOMContentLoaded", function (e) {
       </div>
     </div>
   </form></div></div>`;
-    });
+  });
 
 });
