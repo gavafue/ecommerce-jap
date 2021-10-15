cdd37381779742afa0c99b19bcdf5f17923eaed1
gavafue <88394722+gavafue@users.noreply.github.com>
2021-10-15 08:23:40 -0300

    cambios

--- js/cart.js
+++ js/cart.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
             <td><img src="${element.src}"style="width:60px;"></td>
             <td>${element.name}</td>
             <td>${element.unitCost} ${element.currency}</td>
-            <td><div class="input-group mb-3 mx-auto" style="width:30% !important;"><input type="Number" id="cantidad${[i]}" class="mx-auto form-control" placeholder="Cantidad" value="${element.count}" aria-label="Username" aria-describedby="addon-wrapping" min="0";></div></td>
+            <td><div class="input-group mb-3 mx-auto" style="width:30% !important;"><input type="Number" id="cantidad${[i]}" onchange="modificarSubtotal(${element.unitCost},${i})" class="mx-auto form-control" placeholder="Cantidad" value="${element.count}" aria-label="Username" aria-describedby="addon-wrapping" min="0";></div></td>
             <td>${sub} ${element.currency}</td>
           </tr>  `;
         }
